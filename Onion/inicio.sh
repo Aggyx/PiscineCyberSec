8587076fe4862b9e8bb0328ed16464a4e909c949
SanPy <santi.magniny@gmail.com>
2026-03-29 15:16:02 +0200

    SSH fortification

--- Onion/inicio.sh
+++ Onion/inicio.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
+set -e
+
+# Tengo que crear un directorio para sshd porque sino no arranca dentro de run.
+mkdir -p /run/sshd
+chmod 755 /run/sshd 
+
 /usr/bin/tor -f /etc/tor/torrc & 
-/usr/sbin/sshd -D &
+
+/usr/sbin/sshd -D -e -f /etc/ssh/sshd_config &
+# -D : No daemonizar, se queda en primer plano
+# -e : Enviar los logs a stderr
+# -f : Especificar un archivo de configuración
+
 exec nginx -g 'daemon off;'
